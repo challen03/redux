3343c89b8da4bff0c7c2baf5c3e224d626bb76b9
challen <challen03@126.com>
2016-12-04 19:01:21 +0800

    redux01

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+var commonPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+module.exports = {
+	entry: {index: './src/js/entry.js'},
+	output: {
+		path: './static/',
+		publicPath: 'http://localhost:8080/static/',
+		filename: '[name].js'
+	},
+	module: {
+		loaders: [
+			{test: /\.js$/, loader: 'babel-loader', query: {presets: ['es2015', 'react']}, exclude: /node_modules/},
+			{test: /\.(jpg|png|gif)$/, loader: 'url-loader?limit=8129&name=./[name].[ext]'},
+			{test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},	
+		]
+	},
+    plugins: [commonPlugin]
+}
